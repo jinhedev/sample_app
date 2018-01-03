584159f96e2d58e4fb6b9391ee42a021a523e854
rightmeow <jinhedev@gmail.com>
2018-01-03 11:11:04 -0800

    added new attributes to fixtures and reset db and then re-seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # manual user
-User.create!(name: "ohjinnyboy", email: "example@railstoturial.org", password: "foobarbaz", password_confirmation: "foobarbaz", admin: true)
+User.create!(name: "ohjinnyboy", email: "example@railstoturial.org", password: "foobarbaz", password_confirmation: "foobarbaz", admin: true, activated: true, activated_at: Time.zone.now)
 
 # faker seeds
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "123123123"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
