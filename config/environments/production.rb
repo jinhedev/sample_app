bbed5ca7a035bbd2c6da54ee3a6c249dad34d086
rightmeow <jinhedev@gmail.com>
2018-01-04 16:18:29 -0800

    corrected a typo from stmp to smtp in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
   host = 'sampleapp9000.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.stmp_settings = {
-    :address => 'stmp.sendgrid.net',
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
