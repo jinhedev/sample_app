f1473eaaabc0bf9b8523ce60e9fbc25dc42339dc
rightmeow <jinhedev@gmail.com>
2018-01-06 21:58:37 -0800

    added integration test for root path when logged in

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,7 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost interface" do
     log_in_as(@user)
     get root_path
-    assert_select 'div.pagination'
+    assert_select 'div.pagination' # assert the presence of the pagination class
     # invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: {
@@ -39,6 +39,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # visit different user (no delete links)
     get user_path(users(:sponge))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count.to_s} microposts", response.body
+    # user with zero microposts.
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 
 end
