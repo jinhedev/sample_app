6d92bea0581ee92985769728cb565d755be1a774
rightmeow <jinhedev@gmail.com>
2018-01-07 01:43:26 -0800

    added integration test for micropost picture upload

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination' # assert the presence of the pagination class
+    assert_select 'input[type=submit]' # ???????????????????????????????????????
     # invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: {
@@ -21,13 +22,16 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # valid submisssion
     content = "This is the valid content for a valid micropost."
+    picture = fixture_file_upload('test/fixtures/files/rails.png', 'image/png')
     assert_difference('Micropost.count', 1) do
       post microposts_path, params: {
         micropost: {
-          content: content
+          content: content,
+          picture: picture
         }
       }
     end
+    assert assigns(:micropost).picture? # how does rails know this is that micropost?
     assert_redirected_to root_url
     follow_redirect!
     # delete post
